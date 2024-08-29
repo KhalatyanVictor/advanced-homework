2d6cf09dc1651dc300a68734e74b24858d6586e3
KhalatyanVictor <viktorkhalatyan2008@gmail.com>
2024-08-29 16:21:43 +0400

    Adding FindLongestWord

--- JS/findLongestWord.js
+++ JS/findLongestWord.js
@@ -1,7 +1,22 @@
 // Write a function that identifies the longest word in a given sentence.
 // Examples:
 
-findLongestWord("The quick brown fox jumps over the lazy dog");
-// Output: "jumps"
+function findLongestWord(str) {
+    let words = str.split(" ");
+    let maxCount = 0;
+    let longestWord = "";
+
+    words.forEach(word => {
+        let wordLength = word.length;
+        if (wordLength > maxCount) {
+            maxCount = wordLength;
+            longestWord = word;
+        }
+    });
+
+    console.log(longestWord);
+}
+
+findLongestWord("Hello World!"); // Output: "Hello"
+findLongestWord("The quick brown fox jumps over the lazy dog"); // Output: "jumps"
 findLongestWord("JavaScript is awesome"); // Output: "JavaScript"
-findLongestWord("Hello world!"); // Output: "Hello"
