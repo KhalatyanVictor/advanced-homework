19b92432ec01994ea6cfe26f3787d442faab4f4b
KhalatyanVictor <viktorkhalatyan2008@gmail.com>
2024-10-31 21:46:16 +0400

    Add Timer

---
+++ JS/asyncJS/timer.js
@@ -0,0 +1,26 @@
+let currentTime = document.getElementById("currentTime");
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
+
+let timer;
+let seconds = 0;
+
+startBtn.addEventListener("click", () => {
+  if (!timer) {
+    timer = setInterval(() => {
+      seconds++;
+      currentTime.textContent = seconds;
+    }, 1000);
+  }
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timer);
+  timer = null;
+});
+
+resetBtn.addEventListener("click", () => {
+  clearInterval(timer);
+  currentTime.textContent = 0;
+});
