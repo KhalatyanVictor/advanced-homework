fdd4e628d12467d07c6b7acd09e79c02f1600f00
KhalatyanVictor <viktorkhalatyan2008@gmail.com>
2024-11-07 15:47:54 +0400

    Adding Async Await

---
+++ JS/asyncJS/promises/asyncAwait.js
@@ -0,0 +1,51 @@
+function walkTheDog() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const walked = true;
+      if (walked) {
+        resolve("You walked with DOG!");
+      } else {
+        reject("You didn't walk with DOG!");
+      }
+    }, 500);
+  });
+}
+
+function cleanTheKitchen() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const cleaned = true;
+      if (cleaned) {
+        resolve("You cleaned The Kitchen");
+      } else {
+        reject("You didn't clean The Kitchen");
+      }
+    }, 500);
+  });
+}
+
+function trashOut() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const tookOut = false;
+      if (tookOut) {
+        resolve("You took out the trash");
+      } else {
+        reject("You didn't take out the trash!");
+      }
+    }, 500);
+  });
+}
+
+async function doAllTasks() {
+  const dogRes = await walkTheDog();
+  console.log(dogRes);
+
+  const cleanRes = await cleanTheKitchen();
+  console.log(cleanRes);
+
+  const takeOutTrashRes = await trashOut();
+  console.log(takeOutTrashRes);
+}
+
+console.log(doAllTasks());
